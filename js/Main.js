31cf1e4eb3cd652ecd518d1eea772e222b108849
datracka <datracka@hotmail.com>
2013-05-02 17:45:15 +0200

    Revert "resize elements and delete unused jquery plugin and other minor params." This reverts commit f03072defcb370fa6ac2237dd504213ddaf37de9.
    Conflicts:
    	css/main.css
    	js/Main.js

--- js/Main.js
+++ js/Main.js
@@ -9,7 +9,22 @@ var Main = (function(){
         Main.prototype.fetchElements();
         Main.prototype.initializeEventListeners();
 
-        Main.prototype.init();
+        Main.video = new Video();
+        //set Main.videos with videos from Channel
+        Main.video.loadVideosFromChannel();
+
+        //set Main.aThumbs with thumbs available
+        Main.video.getThumbnails(Main.videos);
+        Main.view = new View();
+        Main.view.drawThumbnailsSidebar(Main.aThumbs, Main.aVideosThumbs);
+
+        //load video
+        Main.video.prepareVideoUrl(Main.videos);
+
+        //TODO: positioning stuff. Move to appropiate place
+        //$("div.loading:nth-child(1)").center();
+        $(".st_thumbs_wrapper").css('left', ($(window).width() - 201) + 'px');
+
 
     }
 
@@ -17,7 +32,8 @@ var Main = (function(){
     Main.library = "";
     Main.video = null;
     Main.view = null;
-    Main.urlVars = null;
+    Main.urlVars = "";
+
     //data -> todo: use Storage library
     Main.aVideosThumbs = []
     Main.aThumbs = []  //array thumbs
@@ -25,40 +41,16 @@ var Main = (function(){
 
     Main.prototype = {
 
-        init: function () {
-
-            //load video if exists Id. else we load it after ask for loadVideosFromChannel.
-            if (typeof(Main.urlVars["id"]) != "undefined"){
-
-                //set and return final url
-                var videoUrl = 'http://www.vimeo.com/' + Main.urlVars["id"];
-
-                var ov = {
-                    videoUrl: videoUrl,
-                    videoTiming: 0 //don't use timing at the moment
-                }
-
-                Video.prototype.getVideo(ov);
-            }
-
-            Main.video = new Video();
-            Main.video.loadVideosFromChannel();
-
-        },
-
         fetchElements: function(){
             var hidedHeader     = $('#hidedHeader')
             var sidebar         = $('#st_thumbs_wrapper')
+            var imgPressed      = $('img[id|="imgThumbs"]')
             var html            = $('html');
-            var cWindow         = $(window);
-
 
             Main.library.set('hidedHeader', hidedHeader);
             Main.library.set('sidebar', sidebar);
-
+            Main.library.set('imgPressed', imgPressed);
             Main.library.set('html', html);
-            Main.library.set('cWindow', cWindow);
-
         },
 
         initializeEventListeners: function () {
@@ -67,31 +59,47 @@ var Main = (function(){
             Main.urlVars = this.getUrlVars();
 
             /** document events **/
-            var cWindow = Main.library.get('cWindow');
             var html = Main.library.get('html');
             var hidedHeader = Main.library.get('hidedHeader');
             var sidebar = Main.library.get('sidebar');
+            var imgPressed = Main.library.get('imgPressed');
 
-
-
-            cWindow.resize(Main.prototype.resizeScreenElements);
+            html.mouseover(Main.onDocument);
 
             sidebar.hover(Main.prototype.showSidebar, Main.prototype.hideSidebar);
-            hidedHeader.hover(Main.prototype.showCloseHeader, Main.prototype.hideCloseHeader);
+            //imgPressed.click(Main.prototype.showVideo)
 
             /** show iframe **/
             $(".wrapper").fadeIn(2000);
 
+            /** set close button animation */
+            $("#closeHeader").animate(
+                {"top": "65px"},
+                "slow",
+                function(){
+                    $("#closeHeader").delay(3000).animate(
+                        {"top": "-65px"},
+                        "slow",
+                        function(){
+                            //enable events
+
+                            hidedHeader.hover(Main.prototype.showCloseHeader, Main.prototype.hideCloseHeader);
+                            //hidedHeader.mouseout(Main.prototype.hideCloseHeader);
+                            hidedHeader.click(Main.prototype.closewindow)
+
+                        }
+                    );
+                }
+            );
         },
 
-        resizeScreenElements: function(){
+        /**
+         * methods realted to events
+         *
+         */
 
-            var elem 			= $('body');
-            var thumbs_wrapper = elem.find('.st_thumbs_wrapper');
-            thumbs_wrapper.css('height', ($(window).height() - 42) + 'px');
-
-            $('.wrapper iframe').css('height', $(window).height() + 'px');
-            $('.wrapper iframe').css('width', $(window).width() + 'px');
+        showVideo: function(){
+            alert("show video");
         },
 
         showSidebar: function () {
@@ -111,6 +119,21 @@ var Main = (function(){
         },
         hideCloseHeader: function () {
             $("#closeHeader").stop().animate({"top": "-65px"}, "fast");
+        },
+
+        closewindow: function(){
+
+            var d = Main.urlVars["a"];
+            var p = Main.urlVars["p"];
+            var url = p + "//" + d;
+
+            var win = parent;
+            win.postMessage("destroy_bookmarklet",url);
+
+        },
+
+        onDocument: function (event){
+             alert("aa");
         },
 
         getUrlVars: function(){
